8c40797f7837633f72c6fa2a02e3aa0bd90a3cb6
nqbao47 <baonguyengau14@gmail.com>
2024-02-20 10:21:29 +0700

    Xử lí dark-mode thêm system

--- src/App.js
+++ src/App.js
@@ -3,8 +3,77 @@ import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import { useColorScheme } from "@mui/material/styles";
 
+import * as React from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Box from "@mui/material/Box";
+
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme();
+
+  const handleChange = (event) => {
+    const selectedMode = event.target.value;
+    setMode(selectedMode);
+  };
+
+  return (
+    <FormControl
+      sx={{ m: 1, minWidth: 120 }}
+      size="small"
+    >
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+        style={{ justifyContent: "center", alignItems: "center" }}
+      >
+        <MenuItem value="light">
+          <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+            <LightModeIcon />
+            Light
+          </div>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              gap: 1,
+            }}
+          >
+            <DarkModeIcon />
+            Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              gap: 2,
+            }}
+          >
+            <SettingsBrightnessIcon />
+            System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
+
 function ModeToggle() {
   const { mode, setMode } = useColorScheme();
+
   return (
     <Button
       onClick={() => {
@@ -19,6 +88,8 @@ function ModeToggle() {
 function App() {
   return (
     <>
+      <ModeSelect />
+      <hr />
       <ModeToggle />
       <hr />
       <div
