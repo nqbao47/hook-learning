ba3a305adb2fdf93c5e569bed58e4652cdd6cec0
nqbao47 <baonguyengau14@gmail.com>
2024-02-20 09:14:08 +0700

    dark_light_mode_v1

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
-import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
+import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    <CssVarsProvider theme={theme}>
       <CssBaseline />
       <App />
-    </ThemeProvider>
+    </CssVarsProvider>
   </React.StrictMode>
 );
